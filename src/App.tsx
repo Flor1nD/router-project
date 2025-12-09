d3b574cb6eb9d12597a2af2bb170325a5308d331
Flor1nD <grudkinkostya2005@gmail.com>
2025-12-09 15:24:31 +0300

    picture

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import PageOne from "./components/pageone.tsx";
 import {useState, useEffect} from "react";
 import axios from 'axios'
 import apiUrl from "./components/api.tsx";
+import picture from "./components/image1.png";
 
 interface Posts {
     userId: number;
@@ -70,7 +71,7 @@ function HomePage() {
                         {posts.slice(0, posts.length).map((post) => (
                             <div key={post.id} className="todo-block">
                                 <p className={"todo-item"}>{post.id}</p>
-                                <img src="https://opis-cdn.tinkoffjournal.ru/mercury/how-to-pay-for-telegram-post-01.okneh0..png" alt="Картинка" loading={"lazy"} style={{width:'250px', height:'250px'}} />
+                                <img src={picture} alt="Картинка" loading={"lazy"} style={{width:'250px', height:'250px'}} />
                                 <h2 className={"todo-item"}>{post.title}</h2>
                                 <p className={"todo-item"}>{post.body}</p>
                                 <button className={"todo-item"} onClick={() => deletePost(post.id)}>Удалить</button>
